5ddd3ed329fa35ff2505c51a9b8b8413ced8ec2f
Afreen Ansari <afreenansari@192.168.1.2>
2023-04-14 02:12:54 +0530

    working on ui

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {BsCheckCircleFill} from "react-icons/bs"
+function Navigation() {
+  return (
+    <>
+    <nav>
+        <ul className='flex gap-[10px] md:gap-[0] flex-wrap md:flex-col'>
+            <li className='flex p-2 gap-2 md:my-[7.5px]  bg-gray-100 rounded-md text-slate-950'><div className='w-6 h-6 rounded-full bg-red-400'></div> High Priority</li>
+            <li className='flex p-2 gap-2 md:my-[7.5px]  bg-gray-100 rounded-md text-slate-950'><div className='w-6 h-6 rounded-full bg-yellow-400'></div> Medium Severity</li>
+            <li className='flex p-2 gap-2 md:my-[7.5px]  bg-gray-100 rounded-md text-slate-950'><div className='w-6 h-6 rounded-full bg-[#66cdaa]'></div> Low Severity</li>
+            <li className='flex p-2 gap-2 md:my-[7.5px]  bg-gray-100 rounded-md text-slate-950'><div className='w-6 h-6 rounded-full bg-white flex items-center justify-center'><BsCheckCircleFill className='text-[#66cdaa] font-bold'/></div> Low Severity</li>
+            <li className='flex p-2 gap-2 md:my-[7.5px]  bg-gray-100 rounded-md text-slate-950'><input type="checkbox" className="checked:bg-blue-500 relative left-1 w-4 mr-2" /> Completed</li>
+        </ul>
+    </nav>
+    </>
+  )
+}
+
+export default Navigation
