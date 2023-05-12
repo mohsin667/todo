23ec34ce8efe22f38826c94f91387f528d4754e7
Mohsin <ansarimohsin667@gmail.com>
2023-05-13 01:06:19 +0530

    ui changes done

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
 
 function Edit({state,setState}) {
-  const handleStae = () => {
-    const findOpen = state.filter(item => {
-      return item.pop === true
-    })
-    let temp = [...state];
-    temp[findOpen[0].id - 1].pop = false 
-    setState(temp)
-  }
   return (
-    <div className='absolute bg-white p-3 right-0 rounded-md top-[30px] shadow-md'>
+    <div className='absolute bg-white p-3 right-0 rounded-md top-[30px] shadow-md z-[99]'>
       <label htmlFor="edit mb-[15px]"> Edit Title
-        <input type='text' className='border rounded-sm mb-5 border-slate-300 mt-1 py-1 px-2' id="edit" />
+        <input type='text' className='border rounded-sm mb-5 border-slate-300 mt-1 py-1 px-2' />
       </label>
 
+      <label className="block mb-5">
+          <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
+          Edit Priority
+          </span>
+          <select className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="you@example.com">
+              <option value="Select Priority">Select Priority</option>
+              <option value="high">High</option>
+              <option value="medium">Medium</option>
+              <option value="low">Low</option>
+          </select>
+      </label>
+      
       <label htmlFor="edit"> Edit Description
-        <input type='text' className='border border-slate-300 rounded-sm mb-5 mt-1 py-1 px-2' id="edit" />
+        <textarea className='border border-slate-300 rounded-sm mb-5 mt-1 py-1 px-2' />
       </label>
 
-      <button className='bg-blue-400 hover:bg-blue-600 text-white px-3 py-2 rounded-md mr-2'>Change</button>
-      <button className='bg-slate-100 hover:bg-slate-600 hover:text-[#fff] px-3 py-2 rounded-md' onClick={handleStae}>Cancel</button>
+      <button className='bg-blue-400 hover:bg-blue-600 text-white px-3 py-2 rounded-md mr-2' >Change</button>
+      <button className='bg-slate-100 hover:bg-slate-600 hover:text-[#fff] px-3 py-2 rounded-md'>Cancel</button>
     </div>
   )
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -9,7 +9,7 @@ function Navigation() {
             <li className='flex p-2 gap-2 md:my-[7.5px]  bg-gray-100 rounded-md text-slate-950'><div className='w-6 h-6 rounded-full bg-yellow-400'></div> Medium Severity</li>
             <li className='flex p-2 gap-2 md:my-[7.5px]  bg-gray-100 rounded-md text-slate-950'><div className='w-6 h-6 rounded-full bg-[#66cdaa]'></div> Low Severity</li>
             <li className='flex p-2 gap-2 md:my-[7.5px]  bg-gray-100 rounded-md text-slate-950'><div className='w-6 h-6 rounded-full bg-white flex items-center justify-center'><BsCheckCircleFill className='text-[#66cdaa] font-bold'/></div> Low Severity</li>
-            <li className='flex p-2 gap-2 md:my-[7.5px]  bg-gray-100 rounded-md text-slate-950'><input type="checkbox" className="checked:bg-blue-500 relative left-1 w-4 mr-2" /> Completed</li>
+            <li className='flex p-2 gap-2 md:my-[7.5px] items-center bg-gray-100 rounded-md text-slate-950 cursor-pointer'> <div style={{background: "#65cdaa"}} className={`w-6 h-6 rounded-full flex items-center justify-center`}><BsCheckCircleFill color="white" /></div>Completed</li>
         </ul>
     </nav>
     </>
